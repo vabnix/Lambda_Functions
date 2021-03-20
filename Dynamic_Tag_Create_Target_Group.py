0d77a649dd49d9a70c43de0083c4249e5709730c
Vaibhav Srivastava <vabnix@hotmail.com>
2021-03-20 02:12:42 -0500

    Create Dynamic_Tag_Create_Target_Group.py

---
+++ Dynamic_Tag_Create_Target_Group.py
@@ -0,0 +1,49 @@
+import boto3
+
+ELB_Client = boto3.client('elbv2')
+
+# List all autoscaling group
+ELB_List = ELB_Client.describe_load_balancers()
+
+ArnList = []
+
+# Now that we have LB list, lets see what we got
+for LB in ELB_List['LoadBalancers']:
+    # with LB we will find TG associated with each LB
+    TG_List = ELB_Client.describe_target_groups()
+    for TargetGroup in TG_List['TargetGroups']:
+        for FormTg in TargetGroup['LoadBalancerArns']:
+            if FormTg == LB['LoadBalancerArn']:
+                ArnList.append(TargetGroup['TargetGroupArn'])
+
+# To avoid duplication
+ArnList = list(set(ArnList))
+
+for Arn in ArnList:
+    describeTag = ELB_Client.describe_tags(
+        ResourceArns=[Arn]
+    )
+    for tagDescription in describeTag['TagDescriptions']:
+        if len(tagDescription['Tags']) == 0:
+            ELB_Client.add_tags(
+                ResourceArns=[Arn],
+                Tags=[
+                    {
+                        'Key': 'Environment',
+                        'Value': 'Dev',
+                    }
+                ],
+            )
+        else:
+            ExistingTags = tagDescription['Tags']
+            KeyTag = [tag['Key'] for tag in ExistingTags]
+            if 'Environment' not in KeyTag:
+                ELB_Client.add_tags(
+                    ResourceArns=[Arn],
+                    Tags=[
+                        {
+                            'Key': 'Environment',
+                            'Value': 'Dev',
+                        }
+                    ],
+                )
